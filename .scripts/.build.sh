092706fd153a301079946deab8f90a380495b394
zephinzer <govtech@joeir.net>
2018-01-23 15:38:30 +0800

    changed [[ to [ for dash

--- .scripts/.build.sh
+++ .scripts/.build.sh
@@ -3,12 +3,12 @@ CURR_DIR=$(dirname $0);
 IMAGE_URL=$1
 IMAGE_TAG=$2;
 BUILD_ARGS=$3;
-if [[ "${IMAGE_URL}" = "" ]]; then
+if [ "${IMAGE_URL}" = "" ]; then
   printf "\n\e[31m\e[1m[!] Specify the namspace to build as the first argument.\e[0m\n";
   exit 1;
 fi;
 
-if [[ "${IMAGE_TAG}" = "" ]]; then
+if [ "${IMAGE_TAG}" = "" ]; then
   printf "\n\e[31m\e[1m[!] Specify the image (directory name) to build as the second argument.\e[0m\n";
   exit 1;
 fi;
