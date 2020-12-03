d30d60ac8646311496069a0d3bbd22db233f6089
Ryan Goh <ryangohjq@gmail.com>
2020-12-03 13:52:21 +0800

    Added other builds, and testing out node matrix for building

--- alpine/.publish.sh
+++ alpine/.publish.sh
@@ -6,24 +6,18 @@ TAG_LATEST="${REPOSITORY_URL}-latest";
 
 VERSIONS=$(docker run --entrypoint="version-info" ${TAG});
 VERSION_ALPINE=$(printf "${VERSIONS}" | grep alpine | cut -f 2 -d ':');
-VERSION_GIT=$(printf "${VERSIONS}" | grep git | cut -f 2 -d ':');
-EXISTENCE_TAG="alpine-${VERSION_ALPINE}";
-EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
 ALPINE_VERSION_REPO_URL="${REPOSITORY_URL}-${VERSION_ALPINE}";
 
-printf "Checking existence of [${EXISTENCE_REPO_URL}]...";
-_="$(docker pull "${EXISTENCE_REPO_URL}")" && EXISTS=$?;
+printf "Checking existence of [${ALPINE_VERSION_REPO_URL}]...";
+_="$(docker pull "${ALPINE_VERSION_REPO_URL}")" && EXISTS=$?;
 if [[ "${EXISTS}" = "0" ]]  && [[ "$*" != *"--force"* ]]; then
-  printf "[${EXISTENCE_REPO_URL}] found. Skipping push.\n";
+  printf "[${ALPINE_VERSION_REPO_URL}] found. Skipping push.\n";
   echo exists;
 else
-  printf "[${EXISTENCE_REPO_URL}] not found. Pushing new image...\n";
+  printf "[${ALPINE_VERSION_REPO_URL}] not found. Pushing new image...\n";
   printf "Pushing [${TAG_LATEST}]... ";
   docker tag ${TAG} ${TAG_LATEST};
   docker push ${TAG_LATEST};
-  printf "Pushing [${EXISTENCE_REPO_URL}]... ";
-  docker tag ${TAG} ${EXISTENCE_REPO_URL};
-  docker push ${EXISTENCE_REPO_URL};
   printf "Pushing [${ALPINE_VERSION_REPO_URL}]... ";
   docker tag ${TAG} ${ALPINE_VERSION_REPO_URL};
   docker push ${ALPINE_VERSION_REPO_URL};
--- ansible/.publish.sh
+++ ansible/.publish.sh
@@ -7,25 +7,19 @@ TAG_LATEST="${REPOSITORY_URL}-latest";
 
 VERSIONS=$(docker run --entrypoint="version-info" ${TAG});
 VERSION_ANSIBLE=$(printf "%s" "${VERSIONS}" | grep ansible: | cut -f 2 -d ':');
-EXISTENCE_TAG="ansible-${VERSION_ANSIBLE}";
-EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
 ANSIBLE_VERSION_REPO_URL="${REPOSITORY_URL}-${VERSION_ANSIBLE}";
 
-printf "Checking existence of [%s]..." "${EXISTENCE_REPO_URL}";
-_="$(docker pull "${EXISTENCE_REPO_URL}")" && EXISTS=$?;
+printf "Checking existence of [%s]..." "${ANSIBLE_VERSION_REPO_URL}";
+_="$(docker pull "${ANSIBLE_VERSION_REPO_URL}")" && EXISTS=$?;
 if [[ "${EXISTS}" = "0" ]]  && [[ "$*" != *"--force"* ]]; then
-  printf "[%s] found. Skipping push.\n" "${EXISTENCE_REPO_URL}";
+  printf "[%s] found. Skipping push.\n" "${ANSIBLE_VERSION_REPO_URL}";
   echo exists;
 else
-  printf "[%s] not found. Pushing new image...\n" "${EXISTENCE_REPO_URL}";
+  printf "[%s] not found. Pushing new image...\n" "${ANSIBLE_VERSION_REPO_URL}";
 
   printf "Pushing [%s]... \n" "${TAG_LATEST}";
   docker tag "${TAG}" "${TAG_LATEST}";
   docker push "${TAG_LATEST}";
-
-  printf "Pushing [%s]... \n" "${EXISTENCE_REPO_URL}";
-  docker tag "${TAG}" "${EXISTENCE_REPO_URL}";
-  docker push "${EXISTENCE_REPO_URL}";
 
   printf "Pushing [%s]... \n" "${ANSIBLE_VERSION_REPO_URL}";
   docker tag "${TAG}" "${ANSIBLE_VERSION_REPO_URL}";
