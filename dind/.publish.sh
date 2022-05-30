830e99b7c2f976d5dfee7b59405a4256a7efed8a
Gyl <test>
2022-05-30 14:51:48 +0800

    updates(dind): switch openjdk to zulu-jdk (still openjdk based); remove python2; update version-infos

--- dind/.publish.sh
+++ dind/.publish.sh
@@ -9,8 +9,11 @@ VERSION_GIT=$(printf "${VERSIONS}" | grep git | cut -f 2 -d ':');
 VERSION_DOCKER=$(printf "${VERSIONS}" | grep 'docker:' | cut -f 2 -d ':');
 VERSION_DOCKER_COMPOSE=$(printf "${VERSIONS}" | grep docker-compose | cut -f 2 -d ':');
 VERSION_OPENSSH=$(printf "${VERSIONS}" | grep openssh | cut -f 2 -d ':');
+VERSION_PYTHON3=$(printf "${VERSIONS}" | grep python | cut -f 2 -d ':');
+VERSION_ZULU_JDK=$(printf "${VERSIONS}" | grep zulu-jdk | cut -f 2 -d ':');
 VERSION_MYSQL_CLIENT=$(printf "${VERSIONS}" | grep mysql-client | cut -f 2 -d ':');
-EXISTENCE_TAG="docker-${VERSION_DOCKER}_docker-compose-${VERSION_DOCKER_COMPOSE}_git-${VERSION_GIT}_openssh-${VERSION_OPENSSH}_mysql-client-${VERSION_MYSQL_CLIENT}";
+
+EXISTENCE_TAG="docker-${VERSION_DOCKER}_docker-compose-${VERSION_DOCKER_COMPOSE}_git-${VERSION_GIT}_python-${VERSION_PYTHON3}_zulujdk-${VERSION_ZULU_JDK}_openssh-${VERSION_OPENSSH}_mysql-client-${VERSION_MYSQL_CLIENT}";
 EXISTENCE_REPO_URL="${REPOSITORY_URL}-${EXISTENCE_TAG}";
 DOCKER_VERSION_REPO_URL="${REPOSITORY_URL}-${VERSION_DOCKER}";
 
